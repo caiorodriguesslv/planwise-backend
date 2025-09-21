255814870d3881ed7c31b61735c1b903804b5253
Caio Rodrigues <caiorodriguesmll@gmail.com>
2025-09-21 16:49:15 -0400

    first commit

---
+++ src/main/java/com/devilish/planwise/PlanwiseApplication.java
@@ -0,0 +1,13 @@
+package com.devilish.planwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devilish/planwise/PlanwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devilish.planwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
